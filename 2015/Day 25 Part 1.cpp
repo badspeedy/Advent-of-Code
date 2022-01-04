3e1183fb22693933de2622c576d09b3f354cb2e8
badspeedy <bad_speedy@yahoo.co.uk>
2022-01-04 20:33:11 +0000

    Create Day 25 Part 1.cpp

---
+++ 2015/Day 25 Part 1.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+int main() {
+
+	int loop = 1;
+	int x_target = 3075, y_target = 2981, x_current = 1, y_current = 1;
+	unsigned long long code = 20151125;
+
+	do {
+
+		code = (code * 252533) % 33554393;
+
+		if (x_current < loop) {
+			x_current++;
+		}
+		else {
+			loop++;
+			x_current = 1;
+		}
+		y_current = loop - x_current + 1;
+    
+	} while ((x_target != x_current) || (y_target != y_current));
+
+	std::cout << code << std::endl;
+
+	return 0;
